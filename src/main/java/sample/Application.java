87a60b6350981917a4f1206abba3d2817c8a08ba
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2018-03-01 22:51:59 +0100

    chore: update to spring boot 2.0.0

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -2,19 +2,12 @@ package sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
