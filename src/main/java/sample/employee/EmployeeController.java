981cb00fe9e86b3ccdd8bc4300ce625522e19844
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2020-06-16 23:23:14 +0200

    feat: add example with rendered column

--- src/main/java/sample/employee/EmployeeController.java
+++ src/main/java/sample/employee/EmployeeController.java
@@ -3,7 +3,6 @@ package sample.employee;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.criteria.*;
 import javax.validation.Valid;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @RestController
 public class EmployeeController {
@@ -35,13 +36,55 @@ public class EmployeeController {
         return employeeRepository.findAll(input, new SalarySpecification(input), new ExcludeAnalystsSpecification());
     }
 
-    private class SalarySpecification implements Specification<Employee> {
+    @RequestMapping(value = "/employees-rendered-column", method = RequestMethod.GET)
+    public DataTablesOutput<Employee> listRendered(@Valid DataTablesInput input) {
+        String searchValue = escapeContent(input.getSearch().getValue());
+        input.getSearch().setValue(""); // prevent search on other fields
+
+        Specification<Employee> fullNameSpecification = (Specification<Employee>) (root, query, criteriaBuilder) -> {
+            if (!hasText(searchValue)) {
+                return null;
+            }
+            String[] parts = searchValue.split(" ");
+            Expression<String> firstNameExpression = criteriaBuilder.lower(root.get("firstName"));
+            Expression<String> lastNameExpression = criteriaBuilder.lower(root.get("lastName"));
+            if (parts.length == 2 && hasText(parts[0]) && hasText(parts[1])) {
+                return criteriaBuilder.or(
+                        criteriaBuilder.and(
+                                criteriaBuilder.equal(firstNameExpression, parts[0]),
+                                criteriaBuilder.like(lastNameExpression, parts[1] + "%", '~')
+                        ),
+                        criteriaBuilder.and(
+                                criteriaBuilder.equal(lastNameExpression, parts[0]),
+                                criteriaBuilder.like(firstNameExpression, parts[1] + "%", '~')
+                        )
+                );
+            } else {
+                return criteriaBuilder.or(
+                        criteriaBuilder.like(firstNameExpression, searchValue + "%", '~'),
+                        criteriaBuilder.like(lastNameExpression, searchValue + "%", '~')
+                );
+            }
+        };
+        return employeeRepository.findAll(input, fullNameSpecification);
+    }
+
+    private String escapeContent(String content) {
+        return content
+                .replaceAll("~", "~~")
+                .replaceAll("%", "~%")
+                .replaceAll("_", "~_")
+                .trim()
+                .toLowerCase();
+    }
+
+    private static class SalarySpecification implements Specification<Employee> {
         private final Integer minSalary;
         private final Integer maxSalary;
 
         SalarySpecification(DataTablesInput input) {
             String salaryFilter = input.getColumn("salary").getSearch().getValue();
-            if (!StringUtils.hasText(salaryFilter)) {
+            if (!hasText(salaryFilter)) {
                 minSalary = maxSalary = null;
                 return;
             }
@@ -51,7 +94,7 @@ public class EmployeeController {
         }
 
         private Integer getValue(String[] bounds, int index) {
-            if (bounds.length > index && StringUtils.hasText(bounds[index])) {
+            if (bounds.length > index && hasText(bounds[index])) {
                 try {
                     return Integer.valueOf(bounds[index]);
                 } catch (NumberFormatException e) {
@@ -76,7 +119,7 @@ public class EmployeeController {
         }
     }
 
-    private class ExcludeAnalystsSpecification implements Specification<Employee> {
+    private static class ExcludeAnalystsSpecification implements Specification<Employee> {
         @Override
         public Predicate toPredicate(Root<Employee> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
             return criteriaBuilder.notEqual(root.get("position"), "Analyst");
